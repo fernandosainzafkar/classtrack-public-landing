b3b48c151065a1bdef8c50176752ff600c51278e
fsainzafkar <fernando.sainz@afkartelecom.com>
2026-08-11 19:32:11 +0200

    Meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -142,6 +142,20 @@ const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
           strategy='afterInteractive'
           src='//js-eu1.hs-scripts.com/149079039.js'
         />
+        <Script id='meta-pixel-init' strategy='afterInteractive'>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1057527790192896');
+          fbq('track', 'PageView');
+        `}
+      </Script>
         <ThemeProvider attribute='class' enableSystem={false} disableTransitionOnChange>
           <TooltipProvider>{children}</TooltipProvider>
         </ThemeProvider>
